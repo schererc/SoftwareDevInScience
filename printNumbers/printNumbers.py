d014df8e0e2d6e47aab2923e31adc18412161892
schererc <christophscherer83@gmail.com>
2022-11-16 13:54:13 +0100

    Revert "printNumbers.py: changed FibonacciSequence to FibonacciRecursion in functionTable"
    This reverts commit 61eae9d4d0315ccbc3de98f0124c7024687b3a2d.

--- printNumbers/printNumbers.py
+++ printNumbers/printNumbers.py
@@ -43,7 +43,7 @@ from functions.factorial import *
 #
 # FUNCTION TABLE
 #
-functionTable = { CONST_FUNC_CODE_FIBONACCI : FibonacciRecursion,
+functionTable = { CONST_FUNC_CODE_FIBONACCI : FibonacciSequence,
                   CONST_FUNC_CODE_FACTORIAL : Factorial,
                 }
 
